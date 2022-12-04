2bcc4d97614566867175ce0adaaa7431c4357cdb
pablogerheim <pablogerheim@gmail.com>
2022-12-04 16:21:29 -0300

    about logo

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,16 @@
-import"./About.css"
+import "./About.css";
 function About() {
-  return<section className="about">
-    <div>
-      <h2>MINHAS EXPERIENCIAS</h2>
-    </div>
-    <div>
-      
-    </div>
-  </section>
+	return (
+		<section className="about">
+			<div className="about__banner">
+				<h2 className="about__title">MINHAS EXPERIÊNCIAS</h2>
+			</div>
+			<div className="about__brands">
+        <div className="about__img__arcelor img"></div>
+				<div className="about__img__montana img"></div>
+        <div className="about__img__copy img"></div>
+			</div>
+		</section>
+	);
 }
-export{About}
+export { About };
