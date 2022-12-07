f3909644757fef07466e25cc8e1040c9b704f5b7
Pablo <pablogerheim@gmail.com>
2022-12-07 16:45:51 -0300

    servicos

--- src/components/Services/Sercives.tsx
+++ src/components/Services/Sercives.tsx
@@ -1,5 +1,24 @@
-import"./Services.css"
+import "./Services.css";
 function Services() {
-  return<section className="services">Services</section>
+	return (
+		<section className="services">
+			<div className="services__banner">
+				<h2 className="services__title">SERVIÇOS</h2>
+			</div>
+			<div className="services__content">
+				<h2 className="service__content__title">SERVIÇOS OFERECIDOS</h2>
+				<div className="services__card">
+					<img src="./src/img/services/s1.png" />
+					<h3 className="service__content__subtitle">Desenvolvimento Font-end</h3>
+					<p>Designes responsivos em React e JS vanila.</p>
+				</div>
+				<div className="services__card">
+					<img src="./src/img/services/s3.png" />
+					<h3 className="service__content__subtitle">Desenvolvimento Back-end</h3>
+					<p>Back end em Node.js e Express.</p>
+				</div>
+			</div>
+		</section>
+	);
 }
-export{Services}
+export { Services };
